257cf0f3f003ba47559a4352c24a66a5fdb6a2f7
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-08-17 16:47:13 +0900

    storyboard を使わず画面実装の練習。

---
+++ NoStoryBoardView/NoStoryBoardView/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NoStoryBoardView
+//
+//  Created by 松田智之 on 2020/08/17.
+//  Copyright © 2020 tmatsuda. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+
+}
+
