b1f75d9d6ee2cc9442b033690f254f0a94535ca6
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-08-22 12:41:24 +0900

    構造体、メソッドまとめ

--- youtubePlayground.playground/Contents.swift
+++ youtubePlayground.playground/Contents.swift
@@ -1,59 +1,56 @@
+// 構造体と関数まとめ１
 
-// 構造体と型
-
-struct Human {
-    var name: String
-    var age: Int
+struct Sphere {
+    static let info = "これは球を扱うための構造体です。"
+    
+    var r:Double
+    
+    init(radius: Double) {
+        self.r = radius
+    }
+    
+    func getVolume() -> Double {
+        let v = (4/3) * Double.pi * r * r * r
+        return v
+    }
+    
+    func getSurface() -> Double {
+        let s = 4 * Double.pi * r * r
+        return s
+    }
 }
-var h = Human(name: "Tomoyuki", age: 27)
-print(h)
-print(type(of: h))
 
-var arr = [10, 20, 30, 40]
-print(arr.count)
+var s = Sphere(radius: 10)
+print(s.getSurface())
+print(s.getVolume())
 
-var ave = 0
+
+
+var arr = [Sphere(radius: 0.1), Sphere(radius: 0.5), Sphere(radius: 1.0), Sphere(radius: 0.02), Sphere(radius: 2.0), Sphere(radius: 3.0)]
+
+var sumS:Double = 0
+
 for i in 0 ..< arr.count {
-    ave = ave + arr[i]
+    sumS = sumS + arr[i].getSurface()
 }
+print(sumS)
 
-ave = ave / arr.count
-print(ave)
+var aveV: Double = 0
+let n:Double = Double(arr.count)
+for i in 0 ..< arr.count {
+    aveV = aveV + arr[i].getVolume()
+}
+aveV = aveV / n
+print(aveV)
 
-struct ClassRoom {
-    var name:String // クラスめい
-    var num:Int // 生徒数
-    var teacher:Human
-    var students:[Human]
-    
-    init(name: String, teacher: Human, students: [Human]) {
-        self.name = name
-        self.teacher = teacher
-        self.students = students
-        self.num = students.count
-    }
-    
-    func classStudentsNameAndAge() {
-        for i in 0 ..< self.students.count {
-            print("生徒名：\(self.students[i].name)、　年齢\(self.students[i].age)")
-        }
+var mb = arr[0].getSurface() / arr[0].getVolume()
+var num = 0
+
+for i in 1 ..< arr.count {
+    var b = arr[i].getSurface() / arr[i].getVolume()
+    if mb < b {
+        num = i
     }
 }
 
-var t = Human(name: "Tom", age: 40)
-var s1 = Human(name: "Alice", age: 15)
-var s2 = Human(name: "Bob", age: 16)
-var s3 = Human(name: "Charly", age: 17)
-
-var sArr = [s1, s2, s3]
-print(type(of: sArr))
-
-var cr = ClassRoom(name: "3-A", teacher: t, students: sArr)
-print(type(of: cr))
-
-print(cr.teacher.name)
-print(cr.students[1].name)
-
-print(cr.num)
-
-cr.classStudentsNameAndAge()
+print("\(arr[num].r)です")
