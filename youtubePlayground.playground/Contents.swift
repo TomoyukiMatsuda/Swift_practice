651e1b53cfec908466026f73ddf4dc597249f663
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-08-24 07:31:25 +0900

    Int型の様々について学習

--- youtubePlayground.playground/Contents.swift
+++ youtubePlayground.playground/Contents.swift
@@ -1,57 +1,20 @@
-// 構造体と関数まとめ１
+import Foundation
 
-struct Sphere {
-    static let info = "これは球を扱うための構造体です。"
+print(sqrt(2))
+print(sqrt(4))
+pow(3, 2)
+
+Int.min
+Int.max
+
+UInt8.max
+
+var x: Double = pow(10, 308)
+x.isInfinite
     
-    var r:Double
-    
-    init(radius: Double) {
-        self.r = radius
-    }
-    
-    func getVolume() -> Double {
-        let v = (4/3) * Double.pi * r * r * r
-        return v
-    }
-    
-    func getSurface() -> Double {
-        let s = 4 * Double.pi * r * r
-        return s
-    }
+if x.isInfinite {
+    print("x は無限大")
+} else {
+    print("無限大でない")
 }
 
-var s = Sphere(radius: 10)
-print(s.getSurface())
-print(s.getVolume())
-
-
-
-var arr = [Sphere(radius: 0.1), Sphere(radius: 0.5), Sphere(radius: 1.0), Sphere(radius: 0.02), Sphere(radius: 2.0), Sphere(radius: 3.0)]
-
-var sumS:Double = 0
-
-for i in 0 ..< arr.count {
-    sumS = sumS + arr[i].getSurface()
-}
-print(sumS)
-
-var aveV: Double = 0
-let n:Double = Double(arr.count)
-for i in 0 ..< arr.count {
-    aveV = aveV + arr[i].getVolume()
-}
-aveV = aveV / n
-print(aveV)
-
-var mb = arr[0].getSurface() / arr[0].getVolume()
-var num = 0
-
-for i in 1 ..< arr.count {
-    let b = arr[i].getSurface() / arr[i].getVolume()
-    if mb < b {
-        num = i
-    }
-}
-
-print("######最終結果################")
-print("\(arr[num].r)です")
