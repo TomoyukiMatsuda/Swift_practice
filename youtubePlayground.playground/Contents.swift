afb2ae65d48035146bdf3f6998904baa3da356dd
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-08-24 08:29:16 +0900

    タプルについて学習

--- youtubePlayground.playground/Contents.swift
+++ youtubePlayground.playground/Contents.swift
@@ -1,14 +1,8 @@
-// 三項演算子
+func divide(value: Double, m: Double, n: Double) -> (m: Double, n: Double) {
+    
+    let mValue = value * m / (m + n)
+    let nValue = value * n / (m + n)
+    return (m: mValue, n: nValue)
+}
 
-var a = 100
-var b = 100
-
-var winner = "勝者は"
-
-winner += a > b ? "Aさんです" : "Bさんです"
-
-print(winner)
-
-winner = a > b ? "勝者はAさんです" : a < b ? "勝者はBさんです" : "引き分けです"
-
-print(winner)
+print(divide(value: 10, m: 2, n: 3))
