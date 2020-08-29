3afd6cbd5b4af62698ec65d88086ed20c1cb663e
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-08-29 11:22:26 +0900

    オプショナル、配列について学習

--- youtubePlayground.playground/Contents.swift
+++ youtubePlayground.playground/Contents.swift
@@ -1,28 +1,46 @@
-// クロージャーの学習、@escaping属性
+// オプショナル
 
-var queue = [() -> Void]()
 
-func enqueue(operation: @escaping () -> Void) {
-    queue.append(operation)
+struct User {
+    
+    var name: String
+    var birthday: String
+    var address:String
+    var phoneNum: String?
+    var mobilePhoneNum: String?
+    
+    func getInfo() -> String{
+        var phrase: String
+        phrase = "氏名: \(self.name)\n"
+        phrase += "生年月日: \(self.birthday)\n"
+        phrase += "住所: \(self.address)\n"
+        phrase += "電話番号: \(self.phoneNum ?? "なし")\n"
+        phrase += "携帯電話番号: \(self.mobilePhoneNum ?? "なし")"
+        
+        return phrase
+    }
 }
 
-enqueue {
-    print("executed")
-}
-enqueue {
-    print("executed")
-}
+var user = User(name: "松田", birthday: "1992-11-24", address: "杉並区だよ")
 
-queue.forEach { $0() }
-print(queue)
+print(user.getInfo())
 
 
-func executeTwice(operation: () -> Void) {
-    operation()
-    operation()
+let Num = 1000
+var n = Num
+var times = 0
+
+while n % 2 == 0 {
+    times += 1
+    n /= 2
+    print(n)
 }
 
-executeTwice {
-    print("実行実行")
-    print("実行")
-}
+print(times)
+
+var j = 5
+
+repeat {
+    print(j)
+    j += 1
+} while j < 10
