22da96bd902fb99e487f363fbe74846b2e46b11f
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-09-07 08:18:08 +0900

    列挙体enumを学習

--- youtubePlayground.playground/Contents.swift
+++ youtubePlayground.playground/Contents.swift
@@ -1,33 +1,15 @@
-// エラー処理 do / catch
 
-import Foundation
+// 列挙体のを学習
 
-func printName(name: String) throws {
-    // nameが空文字の場合にはエラーとなる
-    if name.isEmpty {
-        throw NSError(domain: "error", code: -1, userInfo: nil)
-    }
-    print(name)
-}
-
-do {
-    // エラーの発生する可能性のある処理を記述
-    try printName(name: "")
-    print("処理成功")
-} catch {
-    
-    print(error)
-    print("エラー発生")
+enum Caraquri {
+    case matsuda
+    case kawano
+    case matsumura
+    case morishima
 }
 
 
-//do {
-//    // エラーの発生する可能性のある処理を記述
-//    try printName(name: "") // ここでdo内処理がストップして、catchに移る
-//    print("処理成功")
-//} catch {
-//    // do内 try の処理でエラーが発生した場合の記述
-//    print(error)
-//    print("エラー発生")
-//}
-//try! printName(name: "")
+// 列挙体の要素を取得
+let yakuin = Caraquri.morishima
+
+print(yakuin)
