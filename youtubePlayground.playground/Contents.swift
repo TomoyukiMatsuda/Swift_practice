998d2b874850ef1503ccfa5139fab07b34dec81c
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-09-06 16:39:44 +0900

    エラー処理について学習

--- youtubePlayground.playground/Contents.swift
+++ youtubePlayground.playground/Contents.swift
@@ -1,46 +1,33 @@
-// オプショナル
+// エラー処理 do / catch
 
+import Foundation
 
-struct User {
-    
-    var name: String
-    var birthday: String
-    var address:String
-    var phoneNum: String?
-    var mobilePhoneNum: String?
-    
-    func getInfo() -> String{
-        var phrase: String
-        phrase = "氏名: \(self.name)\n"
-        phrase += "生年月日: \(self.birthday)\n"
-        phrase += "住所: \(self.address)\n"
-        phrase += "電話番号: \(self.phoneNum ?? "なし")\n"
-        phrase += "携帯電話番号: \(self.mobilePhoneNum ?? "なし")"
-        
-        return phrase
+func printName(name: String) throws {
+    // nameが空文字の場合にはエラーとなる
+    if name.isEmpty {
+        throw NSError(domain: "error", code: -1, userInfo: nil)
     }
+    print(name)
 }
 
-var user = User(name: "松田", birthday: "1992-11-24", address: "杉並区だよ")
-
-print(user.getInfo())
-
-
-let Num = 1000
-var n = Num
-var times = 0
-
-while n % 2 == 0 {
-    times += 1
-    n /= 2
-    print(n)
+do {
+    // エラーの発生する可能性のある処理を記述
+    try printName(name: "")
+    print("処理成功")
+} catch {
+    
+    print(error)
+    print("エラー発生")
 }
 
-print(times)
 
-var j = 5
-
-repeat {
-    print(j)
-    j += 1
-} while j < 10
+//do {
+//    // エラーの発生する可能性のある処理を記述
+//    try printName(name: "") // ここでdo内処理がストップして、catchに移る
+//    print("処理成功")
+//} catch {
+//    // do内 try の処理でエラーが発生した場合の記述
+//    print(error)
+//    print("エラー発生")
+//}
+//try! printName(name: "")
